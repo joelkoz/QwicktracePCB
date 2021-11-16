8abc604224b8c7da0feed61089af15bc690fd114
Joel Kozikowski <joel.kozikowski@gmail.com>
2021-11-16 18:23:55 -0500

    Update exposure-control-test.js
    Added code to allow gradual rise of safe light to find best intensity

--- exposure-control-test.js
+++ exposure-control-test.js
@@ -1,6 +1,8 @@
 const pigpioClient = require('pigpio-client');
+var readline = require('readline');
+
 const pigpio = pigpioClient.pigpio({host: '127.0.0.1', port: '8888'});
-// const pigpio = pigpioClient.pigpio({host: '192.168.0.152', port: '8888'});
+// const pigpio = pigpioClient.pigpio({host: '192.168.0.143', port: '8888'});
 
 const ready = new Promise((resolve, reject) => {
   console.log('Attempting to connect to pigpio...');
@@ -12,6 +14,21 @@ function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+readline.emitKeypressEvents(process.stdin);
+
+if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true);
+}
+
+var exitPressed = false;
+
+process.stdin.on('keypress', (chunk, key) => {
+  if (key && key.name == 'x') {
+      exitPressed = true;
+  }
+});
+
+
 ready.then(async (info) => {
   // display information on pigpio and connection status
   console.log(JSON.stringify(info,null,2));
@@ -22,7 +39,7 @@ ready.then(async (info) => {
   console.log('UV Bed on');
   await uv.write(1);  // turn on LED
 
-  await wait(2000);
+  await wait(5000);
 
   console.log('UB Bed off');
 
@@ -32,11 +49,23 @@ ready.then(async (info) => {
   // control Safelight
   const safelight = pigpio.gpio(13);
   await safelight.modeSet('output');
-   
-  let duty = 25;
-  console.log(`Safelight duty level ${duty}`);
-  await safelight.analogWrite(duty);
-  await wait(2000);
+
+  let rise = false;
+  if (rise) {
+      let duty = 25;
+      while (duty < 255 && !exitPressed) {
+        console.log(`Safelight duty level ${duty}`);
+        await safelight.analogWrite(duty);
+        await wait(500);
+        duty += 5;
+      }
+  }
+  else {
+     let duty = 250;
+     console.log(`Safelight duty level ${duty}`);
+     await safelight.analogWrite(duty);
+     await wait(10000);
+  }
 
   console.log('Safelight off')
   await safelight.write(0);
